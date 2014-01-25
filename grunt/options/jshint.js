86e36ef0f534c6ed3d6437adb60e78a825b7726e
Sebastiaan Deckers <seb@ninja.sg>
2014-01-26 02:06:56 +0800

    Fixed JSHint

--- grunt/options/jshint.js
+++ grunt/options/jshint.js
@@ -2,7 +2,11 @@ module.exports = function (grunt) {
   return {
     code: {
       options: grunt.util._.merge({
-        node: true
+        node: true,
+        globals: {
+          require: true,
+          define: true
+        }
       }, grunt.file.readJSON('.jshintrc')),
       src: [
         '*.{json,js}',
